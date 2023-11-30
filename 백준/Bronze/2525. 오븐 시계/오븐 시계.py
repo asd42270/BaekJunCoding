ac9b39c973c89cb64fa5a64e6902913cc087f9db
asd42270 <110414025+asd42270@users.noreply.github.com>
2023-11-30 10:41:09 +0900

    [Bronze III] Title: 오븐 시계, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.py
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.py
@@ -5,16 +5,11 @@ if (m+time)%60==0:
     if h==23:
         print(0+a-1, 0)
     else:
-        if h+a>23:
-            print((h+a)-24,0)
-        else:
-            print(h+a, 0)
+        print((h+a)%24,0)
+
 else:
     a, b = (m+time)//60, (m+time)%60
     if h==23:
         print(0+a-1, b)
     else:
-        if h+a>23:
-            print((h+a)-24, b)
-        else:
-            print(h+a, b)    
+        print((h+a)%24, b)    
