c27ecb7b25366294268a626185aaa1fafba83f39
asd42270 <110414025+asd42270@users.noreply.github.com>
2023-12-06 09:33:21 +0900

    [Bronze III] Title: 최댓값, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.py
+++ 백준/Bronze/2562. 최댓값/최댓값.py
@@ -2,11 +2,7 @@ nums=[]
 for i in range(9):
     num = int(input())
     nums.append(num)
-max=-1
-index = 0
-for i in range(len(nums)):
-    if nums[i] > max:
-        max = nums[i]
-        index = i
-print(max)
-print(index+1)
+
+a = max(nums)
+print(a)
+print(nums.index(a)+1)
