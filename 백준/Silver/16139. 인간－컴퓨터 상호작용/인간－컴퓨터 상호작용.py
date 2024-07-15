f30d6a41f021c2bf550dab4fd4d7717e4e47e9a2
asd42270 <110414025+asd42270@users.noreply.github.com>
2024-07-15 16:01:30 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 116 ms, Memory: 110864 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -5,9 +5,27 @@ string = sys.stdin.readline().rstrip()
 n = int(sys.stdin.readline().rstrip())
 result=[]
 
+# 문자의 위치를 저장할 리스트를 초기화합니다.
+index = {}
+
+# 문자열을 한 번 순회하여 각 문자들의 위치를 저장합니다.
+for i in range(len(string)):
+    if string[i] not in index:
+        index[string[i]] = []
+    index[string[i]].append(i)
+
+
 for _ in range(n):
     a, l, r = map(str,sys.stdin.readline().split())
-    result.append(string[int(l):int(r)+1].count(a))
+    cnt = 0
+
+    # 문자가 문자열에 존재하는 경우에만 처리합니다.
+    if a in index:
+        for i in index[a]:
+            if int(l) <= i and i <= int(r):
+                cnt += 1
+
+    result.append(cnt)
 
 for elem in result:
     print(elem)
